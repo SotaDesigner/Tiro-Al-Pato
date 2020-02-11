57267511eb33bf40c1b9bdff519b928333288578
Jose Antonio Diaz Llamas <43948798+SotaDesigner@users.noreply.github.com>
2020-02-11 12:19:02 +0100

    pepe

--- Assets/Scripts/Nube.cs
+++ Assets/Scripts/Nube.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Destruir : MonoBehaviour
+public class Nube : MonoBehaviour
 {
     Animator mA;
     [SerializeField]
-    
+    public int masMunicion;
     private void Start()
     {
         mA = GetComponent<Animator>();
     }
-
+    private void OnMouseDown()
+    {
+        GameControl.municion += masMunicion;
+        Destroy(gameObject);
+    }
     /// <summary>
     /// Llamar desde la animación
     /// </summary>
